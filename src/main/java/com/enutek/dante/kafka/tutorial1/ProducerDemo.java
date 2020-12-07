433497c5726c8777586e7e0f4fb235a51c03cd03
dante-byte <559.dantewhite@gmail.com>
2020-12-07 08:30:54 -0500

    gjkhgcf

--- src/main/java/com/enutek/dante/kafka/tutorial1/ProducerDemo.java
+++ src/main/java/com/enutek/dante/kafka/tutorial1/ProducerDemo.java
@@ -1,6 +1,8 @@
 package com.enutek.dante.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -10,16 +12,33 @@ public class ProducerDemo {
 
     public static void main(String[] args) {
 
-        // create producer properties
+        //producer properties
 
 
         String bootstrapServers = "127.0.0.1:9092";
 
+        //producer properties
         Properties  properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName()); // what type of value you're send to kafka in bytes // string serializer needed // key will be a string serialzier
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        properties.setProperty("key.serializer", StringSerializer.class.getName()); // what type of value you're send to kafka in bytes // string serializer needed // key will be a string serialzier
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
+        // kafka producer
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //producer record
+
+        ProducerRecord<String, String> record =
+                new ProducerRecord<String, String>("first_topic","hello-world-test");
+
+
+        // send data
+
+        producer.send(record);
+        producer.flush();
+        producer.close();
+
 
 
     }
